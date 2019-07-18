44d1886b5714b8a75179386db2e44742ec6a4107
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-07-18 02:28:19 -0700

    Add a python version of exercise4 to take advantage of a debugging environment

---
+++ Exercise4 - Neural Networks Learning/exercise4.py
@@ -0,0 +1,237 @@
+"""
+Moved python code from Jupyter Notebook to a python file
+to be able to use PyCharm's debugging environment.
+Code is identical to the one in Jupyter Notebook.
+"""
+
+import os
+import numpy as np
+from matplotlib import pyplot
+from scipy import optimize
+from scipy.io import loadmat
+import utils
+
+# define the submission/grader object for this exercise
+grader = utils.Grader()
+
+#  training data stored in arrays X, y
+data = loadmat(os.path.join('Data', 'ex4data1.mat'))
+X, y = data['X'], data['y'].ravel()
+
+# set the zero digit to 0, rather than its mapped 10 in this dataset
+# This is an artifact due to the fact that this dataset was used in
+# MATLAB where there is no index 0
+y[y == 10] = 0
+
+# Number of training examples
+m = y.size
+
+# Randomly select 100 data points to display
+rand_indices = np.random.choice(m, 100, replace=False)
+sel = X[rand_indices, :]
+
+utils.displayData(sel)
+
+# Setup the parameters you will use for this exercise
+input_layer_size  = 400  # 20x20 Input Images of Digits
+hidden_layer_size = 25   # 25 hidden units
+num_labels = 10          # 10 labels, from 0 to 9
+
+# Load the weights into variables Theta1 and Theta2
+weights = loadmat(os.path.join('Data', 'ex4weights.mat'))
+
+# Theta1 has size 25 x 401
+# Theta2 has size 10 x 26
+Theta1, Theta2 = weights['Theta1'], weights['Theta2']
+
+# swap first and last columns of Theta2, due to legacy from MATLAB indexing,
+# since the weight file ex3weights.mat was saved based on MATLAB indexing
+Theta2 = np.roll(Theta2, 1, axis=0)
+
+# Unroll parameters
+nn_params = np.concatenate([Theta1.ravel(), Theta2.ravel()])
+
+
+def nnCostFunction(nn_params,
+                   input_layer_size,
+                   hidden_layer_size,
+                   num_labels,
+                   X, y, lambda_=0.0):
+    """
+    Implements the neural network cost function and gradient for a two layer neural
+    network which performs classification.
+
+    Parameters
+    ----------
+    nn_params : array_like
+        The parameters for the neural network which are "unrolled" into
+        a vector. This needs to be converted back into the weight matrices Theta1
+        and Theta2.
+
+    input_layer_size : int
+        Number of features for the input layer.
+
+    hidden_layer_size : int
+        Number of hidden units in the second layer.
+
+    num_labels : int
+        Total number of labels, or equivalently number of units in output layer.
+
+    X : array_like
+        Input dataset. A matrix of shape (m x input_layer_size).
+
+    y : array_like
+        Dataset labels. A vector of shape (m,).
+
+    lambda_ : float, optional
+        Regularization parameter.
+
+    Returns
+    -------
+    J : float
+        The computed value for the cost function at the current weight values.
+
+    grad : array_like
+        An "unrolled" vector of the partial derivatives of the concatenatation of
+        neural network weights Theta1 and Theta2.
+
+    Instructions
+    ------------
+    You should complete the code by working through the following parts.
+
+    - Part 1: Feedforward the neural network and return the cost in the
+              variable J. After implementing Part 1, you can verify that your
+              cost function computation is correct by verifying the cost
+              computed in the following cell.
+
+    - Part 2: Implement the backpropagation algorithm to compute the gradients
+              Theta1_grad and Theta2_grad. You should return the partial derivatives of
+              the cost function with respect to Theta1 and Theta2 in Theta1_grad and
+              Theta2_grad, respectively. After implementing Part 2, you can check
+              that your implementation is correct by running checkNNGradients provided
+              in the utils.py module.
+
+              Note: The vector y passed into the function is a vector of labels
+                    containing values from 0..K-1. You need to map this vector into a
+                    binary vector of 1's and 0's to be used with the neural network
+                    cost function.
+
+              Hint: We recommend implementing backpropagation using a for-loop
+                    over the training examples if you are implementing it for the
+                    first time.
+
+    - Part 3: Implement regularization with the cost function and gradients.
+
+              Hint: You can implement this around the code for
+                    backpropagation. That is, you can compute the gradients for
+                    the regularization separately and then add them to Theta1_grad
+                    and Theta2_grad from Part 2.
+
+    Note
+    ----
+    We have provided an implementation for the sigmoid function in the file
+    `utils.py` accompanying this assignment.
+    """
+    # Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
+    # for our 2 layer neural network
+    Theta1 = np.reshape(nn_params[:hidden_layer_size * (input_layer_size + 1)],
+                        (hidden_layer_size, (input_layer_size + 1)))
+
+    Theta2 = np.reshape(nn_params[(hidden_layer_size * (input_layer_size + 1)):],
+                        (num_labels, (hidden_layer_size + 1)))
+
+    # Setup some useful variables
+    m = y.size
+
+    # You need to return the following variables correctly
+    J = 0
+    Theta1_grad = np.zeros(Theta1.shape)
+    Theta2_grad = np.zeros(Theta2.shape)
+
+    # ====================== YOUR CODE HERE ======================
+
+    # Part 1 - Forward Propagation and Cost Calculation
+
+    # Forward Propagation
+    X = np.hstack((np.ones((m, 1)), X))
+    a1 = X.T  # input layer
+    a2 = utils.sigmoid(Theta1 @ a1)
+    a2 = np.vstack((np.ones((1, a2.shape[1])), a2))
+    a3 = utils.sigmoid(Theta2 @ a2)  # hypothesis (K x m) matrix
+
+    # Convert y into properly indexed (K x m) matrix.
+    new_y = np.zeros((a3.T.shape))
+    new_y[np.arange(new_y.shape[0]), y] = 1
+    y = new_y.T
+
+    # Cost Calculation
+    r = (lambda_ / (2 * m)) * (np.sum(Theta1[:, 1:] ** 2) + np.sum(Theta2[:, 1:] ** 2))
+    J = (1 / m) * np.sum((-y) * np.log(a3) - (1 - y) * np.log(1 - a3)) + r
+
+    # ================================================================
+    # Unroll gradients
+    # grad = np.concatenate([Theta1_grad.ravel(order=order), Theta2_grad.ravel(order=order)])
+    grad = np.concatenate([Theta1_grad.ravel(), Theta2_grad.ravel()])
+
+    return J, grad
+
+
+# --------------Testing Cost Function w/o Regularization -----------------
+lambda_ = 0
+J, _ = nnCostFunction(nn_params, input_layer_size, hidden_layer_size,
+                   num_labels, X, y, lambda_)
+print('Cost at parameters (loaded from ex4weights): %.6f ' % J)
+print('The cost should be about                   : 0.287629.')
+
+grader = utils.Grader()
+grader[1] = nnCostFunction
+grader.grade()
+
+# --------------Testing Cost Function w/ Regularization -------------------
+lambda_ = 1
+J, _ = nnCostFunction(nn_params, input_layer_size, hidden_layer_size,
+                      num_labels, X, y, lambda_)
+
+print('Cost at parameters (loaded from ex4weights): %.6f' % J)
+print('This value should be about                 : 0.383770.')
+
+grader[2] = nnCostFunction
+grader.grade()
+
+
+def sigmoidGradient(z):
+    """
+    Computes the gradient of the sigmoid function evaluated at z.
+    This should work regardless if z is a matrix or a vector.
+    In particular, if z is a vector or matrix, you should return
+    the gradient for each element.
+
+    Parameters
+    ----------
+    z : array_like
+        A vector or matrix as input to the sigmoid function.
+
+    Returns
+    --------
+    g : array_like
+        Gradient of the sigmoid function. Has the same shape as z.
+
+    Instructions
+    ------------
+    Compute the gradient of the sigmoid function evaluated at
+    each value of z (z can be a matrix, vector or scalar).
+
+    Note
+    ----
+    We have provided an implementation of the sigmoid function
+    in `utils.py` file accompanying this assignment.
+    """
+
+    g = np.zeros(z.shape)
+
+    # ====================== YOUR CODE HERE ======================
+
+
+
+    # =============================================================
+    return g
