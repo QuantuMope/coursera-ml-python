1be0bcf11050c33bdacea2d85c5b4e954ad53d87
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-07-24 20:07:06 -0700

    Finish exercise6.

--- Exercise6 - Support Vector Machines/exercise6.py
+++ Exercise6 - Support Vector Machines/exercise6.py
@@ -9,6 +9,7 @@ import utils
 # define the submission/grader object for this exercise
 grader = utils.Grader()
 
+# Functions with completed code.
 
 def gaussianKernel(x1, x2, sigma):
     """
@@ -91,12 +92,16 @@ def dataset3Params(X, y, Xval, yval):
         np.mean(predictions != yval)
     """
     # You need to return the following variables correctly.
+    C = 1
+    sigma = 0.3
+
+    # ====================== YOUR CODE HERE ======================
+
+    # Range of C and sigma values to be tested.
     C_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
     sigma_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
     best_C = 0.01
     best_sigma = 0.01
-
-    # ====================== YOUR CODE HERE ======================
 
     # First iteration to simply set best_error.
     model = utils.svmTrain(X, y, best_C, gaussianKernel, args=(best_sigma,))
@@ -121,6 +126,180 @@ def dataset3Params(X, y, Xval, yval):
     # ============================================================
     return best_C, best_sigma
 
+
+def processEmail(email_contents, verbose=True):
+    """
+    Preprocesses the body of an email and returns a list of indices
+    of the words contained in the email.
+
+    Parameters
+    ----------
+    email_contents : str
+        A string containing one email.
+
+    verbose : bool
+        If True, print the resulting email after processing.
+
+    Returns
+    -------
+    word_indices : list
+        A list of integers containing the index of each word in the
+        email which is also present in the vocabulary.
+
+    Instructions
+    ------------
+    Fill in this function to add the index of word to word_indices
+    if it is in the vocabulary. At this point of the code, you have
+    a stemmed word from the email in the variable word.
+    You should look up word in the vocabulary list (vocabList).
+    If a match exists, you should add the index of the word to the word_indices
+    list. Concretely, if word = 'action', then you should
+    look up the vocabulary list to find where in vocabList
+    'action' appears. For example, if vocabList[18] =
+    'action', then, you should add 18 to the word_indices
+    vector (e.g., word_indices.append(18)).
+
+    Notes
+    -----
+    - vocabList[idx] returns a the word with index idx in the vocabulary list.
+
+    - vocabList.index(word) return index of word `word` in the vocabulary list.
+      (A ValueError exception is raised if the word does not exist.)
+    """
+    # Load Vocabulary
+    vocabList = utils.getVocabList()
+
+    # Init return value
+    word_indices = []
+
+    # ========================== Preprocess Email ===========================
+    # Find the Headers ( \n\n and remove )
+    # Uncomment the following lines if you are working with raw emails with the
+    # full headers
+    # hdrstart = email_contents.find(chr(10) + chr(10))
+    # email_contents = email_contents[hdrstart:]
+
+    # Lower case
+    email_contents = email_contents.lower()
+
+    # Strip all HTML
+    # Looks for any expression that starts with < and ends with > and replace
+    # and does not have any < or > in the tag it with a space
+    email_contents = re.compile('<[^<>]+>').sub(' ', email_contents)
+
+    # Handle Numbers
+    # Look for one or more characters between 0-9
+    email_contents = re.compile('[0-9]+').sub(' number ', email_contents)
+
+    # Handle URLS
+    # Look for strings starting with http:// or https://
+    email_contents = re.compile('(http|https)://[^\s]*').sub(' httpaddr ', email_contents)
+
+    # Handle Email Addresses
+    # Look for strings with @ in the middle
+    email_contents = re.compile('[^\s]+@[^\s]+').sub(' emailaddr ', email_contents)
+
+    # Handle $ sign
+    email_contents = re.compile('[$]+').sub(' dollar ', email_contents)
+
+    # get rid of any punctuation
+    email_contents = re.split('[ @$/#.-:&*+=\[\]?!(){},''">_<;%\n\r]', email_contents)
+
+    # remove any empty word string
+    email_contents = [word for word in email_contents if len(word) > 0]
+
+    # Stem the email contents word by word
+    stemmer = utils.PorterStemmer()
+    processed_email = []
+    for word in email_contents:
+        # Remove any remaining non alphanumeric characters in word
+        word = re.compile('[^a-zA-Z0-9]').sub('', word).strip()
+        word = stemmer.stem(word)
+        processed_email.append(word)
+
+        if len(word) < 1:
+            continue
+
+        # Look up the word in the dictionary and add to word_indices if found
+        # ====================== YOUR CODE HERE ======================
+
+        if word in vocabList:
+            addition = vocabList.index(word)
+            word_indices.append(addition)
+
+        # =============================================================
+
+    if verbose:
+        print('----------------')
+        print('Processed email:')
+        print('----------------')
+        print(' '.join(processed_email))
+    return word_indices
+
+
+def emailFeatures(word_indices):
+    """
+    Takes in a word_indices vector and produces a feature vector from the word indices.
+
+    Parameters
+    ----------
+    word_indices : list
+        A list of word indices from the vocabulary list.
+
+    Returns
+    -------
+    x : list
+        The computed feature vector.
+
+    Instructions
+    ------------
+    Fill in this function to return a feature vector for the
+    given email (word_indices). To help make it easier to  process
+    the emails, we have have already pre-processed each email and converted
+    each word in the email into an index in a fixed dictionary (of 1899 words).
+    The variable `word_indices` contains the list of indices of the words
+    which occur in one email.
+
+    Concretely, if an email has the text:
+
+        The quick brown fox jumped over the lazy dog.
+
+    Then, the word_indices vector for this text might look  like:
+
+        60  100   33   44   10     53  60  58   5
+
+    where, we have mapped each word onto a number, for example:
+
+        the   -- 60
+        quick -- 100
+        ...
+
+    Note
+    ----
+    The above numbers are just an example and are not the actual mappings.
+
+    Your task is take one such `word_indices` vector and construct
+    a binary feature vector that indicates whether a particular
+    word occurs in the email. That is, x[i] = 1 when word i
+    is present in the email. Concretely, if the word 'the' (say,
+    index 60) appears in the email, then x[60] = 1. The feature
+    vector should look like:
+        x = [ 0 0 0 0 1 0 0 0 ... 0 0 0 0 1 ... 0 0 0 1 0 ..]
+    """
+    # Total number of words in the dictionary
+    n = 1899
+
+    # You need to return the following variables correctly.
+    x = np.zeros(n)
+
+    # ===================== YOUR CODE HERE ======================
+
+    for index in word_indices:
+        x[index] = 1
+
+    # ===========================================================
+
+    return x
 
 # -------------------------- Testing Gaussian Kernel --------------------------------------
 
@@ -178,20 +357,113 @@ X, y, Xval, yval = data['X'], data['y'][:, 0], data['Xval'], data['yval'][:, 0]
 # Plot training data
 utils.plotData(X, y)
 
-# Try different SVM Parameters here
-C, sigma = dataset3Params(X, y, Xval, yval)
-
-# Train the SVM
-# model = utils.svmTrain(X, y, C, lambda x1, x2: gaussianKernel(x1, x2, sigma))
-model = utils.svmTrain(X, y, C, gaussianKernel, args=(sigma,))
-utils.visualizeBoundary(X, y, model)
-print(C, sigma)
-
-grader[2] = lambda : (C, sigma)
-grader.grade()
+# # Try different SVM Parameters here
+# C, sigma = dataset3Params(X, y, Xval, yval)
+#
+# # Train the SVM
+# # model = utils.svmTrain(X, y, C, lambda x1, x2: gaussianKernel(x1, x2, sigma))
+# model = utils.svmTrain(X, y, C, gaussianKernel, args=(sigma,))
+# utils.visualizeBoundary(X, y, model)
+# print(C, sigma)
+#
+# grader[2] = lambda : (C, sigma)
+# grader.grade()
 
 # ---------------------- Testing Email Processing ------------------------------------------
 
+#  To use an SVM to classify emails into Spam v.s. Non-Spam, you first need
+#  to convert each email into a vector of features. In this part, you will
+#  implement the preprocessing steps for each email. You should
+#  complete the code in processEmail.m to produce a word indices vector
+#  for a given email.
 
+# Extract Features
+with open(os.path.join('Data', 'emailSample1.txt')) as fid:
+    file_contents = fid.read()
+
+word_indices = processEmail(file_contents)
+
+#Print Stats
+print('-------------')
+print('Word Indices:')
+print('-------------')
+print(word_indices)
+
+grader[3] = processEmail
+grader.grade()
+
+# ---------------------- Testing Email Feature Extraction ------------------------------------------
+
+# Extract Features
+with open(os.path.join('Data', 'emailSample1.txt')) as fid:
+    file_contents = fid.read()
+
+word_indices  = processEmail(file_contents)
+features      = emailFeatures(word_indices)
+
+# Print Stats
+print('\nLength of feature vector: %d' % len(features))
+print('Number of non-zero entries: %d' % sum(features > 0))
+
+grader[4] = emailFeatures
+grader.grade()
+
+# -----------------------------------------------------------------------------------------------------
+
+# Load the Spam Email dataset
+# You will have X, y in your environment
+data = loadmat(os.path.join('Data', 'spamTrain.mat'))
+X, y= data['X'].astype(float), data['y'][:, 0]
+
+print('Training Linear SVM (Spam Classification)')
+print('This may take 1 to 2 minutes ...\n')
+
+C = 0.1
+model = utils.svmTrain(X, y, C, utils.linearKernel)
+
+# Compute the training accuracy
+p = utils.svmPredict(model, X)
+
+print('Training Accuracy: %.2f' % (np.mean(p == y) * 100))
+
+# Load the test dataset
+# You will have Xtest, ytest in your environment
+data = loadmat(os.path.join('Data', 'spamTest.mat'))
+Xtest, ytest = data['Xtest'].astype(float), data['ytest'][:, 0]
+
+print('Evaluating the trained Linear SVM on a test set ...')
+p = utils.svmPredict(model, Xtest)
+
+print('Test Accuracy: %.2f' % (np.mean(p == ytest) * 100))
+
+# Sort the weights and obtin the vocabulary list
+# NOTE some words have the same weights,
+# so their order might be different than in the text above
+idx = np.argsort(model['w'])
+top_idx = idx[-15:][::-1]
+vocabList = utils.getVocabList()
+
+print('Top predictors of spam:')
+print('%-15s %-15s' % ('word', 'weight'))
+print('----' + ' '*12 + '------')
+for word, w in zip(np.array(vocabList)[top_idx], model['w'][top_idx]):
+    print('%-15s %0.2f' % (word, w))
+
+# ---------------------- Trying own Emails (optional) ------------------------------------------
+
+filename = os.path.join('Data', 'emailSample1.txt')
+
+with open(filename) as fid:
+    file_contents = fid.read()
+
+word_indices = processEmail(file_contents, verbose=False)
+x = emailFeatures(word_indices)
+p = utils.svmPredict(model, x)
+
+print('\nProcessed %s\nSpam Classification: %s' % (filename, 'spam' if p else 'not spam'))
+
+# ---------------------- Building your own Dataset (optional) --------------------------------------
+
+# Have yet to complete.
 
 print('Debug Breakpoint')
